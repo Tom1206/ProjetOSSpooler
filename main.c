14fcc571fa9a9e0e671ff27131ec421d4ecaacb0
wael adnane <waem.adnan.hamed@gmail.com>
2017-10-18 14:40:41 +0200

    Modification du makefile : ajout de la commande -Werror

--- main.c
+++ main.c
@@ -4,5 +4,8 @@ int main(int argc, char const *argv[]) {
     (void)argc;
     (void)argv;
     printf("Hello World\n");
+
+    //affiche le contenu d'un répertoire toutes les secondes
+    
     return 0;
 }
--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 CC= gcc
-CFLAGS= -W -Wall
+CFLAGS= -W -Wall -Werror
 LDFLAGS=
 EXEC= main.out
 SRC= $(wildcard *.c)
